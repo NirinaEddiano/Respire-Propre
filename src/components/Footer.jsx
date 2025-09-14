b04abe4dd973e53bd51867659f47c69f5c2b3906
NirinaEddiano <anoeddi84@gmail.com>
2025-09-14 20:04:28 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -53,6 +53,19 @@ function Footer() {
 
       </div>
 
+      <div className="footer-map">
+        <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d174830.2285117281!2d-71.3913709320875!3d46.85233316443429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb8968a05db8893%3A0x8fc52d63f0e83a03!2sQu%C3%A9bec%2C%20QC!5e0!3m2!1sfr!2sca!4v1663273188123!5m2!1sfr!2sca" 
+          width="100%" 
+          height="350" 
+          style={{ border: 0 }} 
+          allowFullScreen="" 
+          loading="lazy" 
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Carte de la ville de Québec">
+        </iframe>
+      </div>
+
       <div className="footer-bottom">
         <p>&copy; {new Date().getFullYear()} Saison + . Tous droits réservés.</p>
       </div>
